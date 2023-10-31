8c71c5665c7c6cd09ff2c212f49adb0f239b1ac9
dr0th3r <dr0th3r.code@gmail.com>
2023-10-31 21:48:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# DO THIS
+
+- change the url
+
+- change name at the top
+
+- change the lection
+
+- change "Hmaty" to "Vety"
+
+- change the time period for which you write
+
+- change anything else you'd like using dev tools (F12 or Ctrl + Shift + I)
---
+++ script.js
@@ -0,0 +1,152 @@
+texts = {
+  1: "",
+  2: "",
+  5: "dusil sil, klid klidu, dusil sil, dusili dusil dusil sil, klasik klasik, radil jsi laik, laiků, dusili sili, laik, laiků, klasik, klasiků, radil jsi, klid klidu, lidu, radil jsi, lid lidu, dusili dusil sil, klid klidu, dali ji, dusil",
+  14: `úhrady dluhů, útoky tisku, údaje o skladu, túry do hor, sklidili úrodu, dostal úroky, úklid parku, hledali úspory, úkoly politika, úloha kritiky, starty letadel, start rakety, dostal úkoly, podepsal dopisy, podepsal úpis, platil útratu, odeslali úhradu tohoto dluhu, dodali údaje pro statistiku, dostali jste údaje o odletu letadla, starali se o úsek lesů, urgujete tapety, pojistil úspory, urgujete laky a pasty, dopsali jste údaje do tiskopisu, prodali jste úrodu okurek, úhrady dluhů, útoky tisku, údaje o skladu, túry do hor, sklidili úrodu, dostal úroky, úklid parku, hledali úspory, úkoly politika, úloha kritiky, starty letadel, start rakety, dostal úkoly, podepsal dopisy, podepsal úpis, platil útratu, odeslali úhradu tohoto dluhu, dodali údaje pro statistiku, dostali jste údaje o odletu letadla, starali se o úsek lesů, urgujete tapety, pojistil úspory, urgujete laky a pasty, dopsali jste údaje do tiskopisu, prodali jste úrodu okurek, úhrady dluhů, útoky tisku, údaje o skladu, túry do hor, sklidili úrodu, dostal úroky, úklid parku, hledali úspory, úkoly politika, úloha kritiky, starty letadel, start rakety, dostal úkoly, podepsal dopisy, podepsal úpis, platil útratu, odeslali úhradu tohoto dluhu, dodali údaje pro statistiku, dostali jste údaje o odletu letadla, starali se o úsek lesů, urgujete tapety, pojistil úspory, urgujete laky a pasty, dopsali jste údaje do tiskopisu, prodali jste úrodu okurek,`,
+};
+
+letters = {
+  1: [],
+  2: [],
+  5: ["i", ","],
+  14: ["ú"],
+};
+
+function getRandomArrayEl(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function setWrongTexts(lectionId, wrongChar) {
+  const baseText = texts[lectionId];
+
+  const finalTexts = [];
+  let tmpText = "";
+
+  baseText.split(" ").forEach((text) => {
+    if (tmpText.length + text.length < 75) {
+      tmpText += ` ${text}`;
+    } else {
+      finalTexts.push(tmpText.trim());
+      tmpText = text;
+    }
+  });
+
+  const wrongCharPlace = [finalTexts[2].indexOf(wrongChar), 2];
+
+  const textDiv = document.createElement("div");
+
+  let charCount = 0;
+  finalTexts.forEach((text, id) => {
+    const wrongText = document.createElement("p");
+    const wrongCountEl = document.createElement("p");
+    wrongCountEl.textContent = "1";
+    wrongCountEl.id = "wrong__count__bottom";
+
+    if (id == wrongCharPlace[1]) {
+      const leadingSpace = document.createElement("span");
+      leadingSpace.style.color = "transparent";
+      leadingSpace.textContent = "a".repeat(wrongCharPlace[0] - 1);
+      wrongText.appendChild(leadingSpace);
+      wrongText.innerHTML += wrongChar;
+    } else {
+      wrongText.textContent = "";
+    }
+    const correctText = document.createElement("p");
+    correctText.textContent = text;
+    wrongText.id = "cur__text";
+    correctText.id = "cur__text";
+
+    charCount += correctText.textContent.length;
+
+    const charCountEl = document.createElement("p");
+    charCountEl.textContent = charCount;
+    charCountEl.id = "char__count__bottom";
+
+    textDiv.appendChild(wrongText);
+    if (id == wrongCharPlace[1]) {
+      textDiv.appendChild(wrongCountEl);
+    }
+    textDiv.appendChild(charCountEl);
+    textDiv.appendChild(correctText);
+  });
+
+  const bottomDiv = document.getElementById("bottom");
+  bottomDiv.innerHTML = textDiv.innerHTML;
+}
+
+function setCorrectTexts(lectionId) {
+  const baseText = texts[lectionId];
+
+  const bottomDiv = document.getElementById("bottom");
+  bottomDiv.innerHTML = "";
+
+  let tmpText = "";
+  let charCount = 0;
+
+  baseText.split(" ").forEach((text) => {
+    if (tmpText.length + text.length < 75) {
+      tmpText += ` ${text}`;
+    } else {
+      const textEl = document.createElement("p");
+      textEl.textContent = tmpText.trim();
+      textEl.id = "cur__text";
+
+      charCount += textEl.textContent.length;
+
+      const charCountEl = document.createElement("p");
+      charCountEl.textContent = charCount;
+      charCountEl.id = "char__count__bottom";
+
+      const emptyEl = document.createElement("p");
+      emptyEl.id = "cur__text";
+
+      bottomDiv.appendChild(emptyEl);
+      bottomDiv.appendChild(charCountEl);
+      bottomDiv.appendChild(textEl);
+
+      tmpText = text;
+    }
+  });
+}
+
+function generateResults() {
+  const lection = document.querySelector(".lection");
+  const lectionId = parseInt(lection.value);
+
+  console.log(lectionId);
+
+  const wrongCountEl = document.getElementById("wrong__count");
+  const speedEl = document.getElementById("speed");
+  const wrongnessEl = document.getElementById("wrongness");
+  const wrongCharsEl = document.getElementById("wrong__chars");
+  const slowestCharsEl = document.getElementById("slowest__chars");
+
+  const wrongCount = getRandomInt(0, 2);
+  const speed = getRandomInt(100, 250);
+  //const wrongness = (speed * 3) / wrongCount;
+
+  wrongCountEl.textContent = `Počet chyb: ${wrongCount}`;
+  speedEl.textContent = `Rychlost: ${speed} [úh/min]`;
+  if (wrongCount > 0) {
+    const wrongness = (wrongCount / (speed * 3)) * 90;
+    const wrongChar = getRandomArrayEl(letters[lectionId]);
+    wrongnessEl.textContent = `Chybovost: ${wrongness.toFixed(2)}`;
+    wrongCharsEl.textContent = `Chybné znaky: ${wrongChar}`;
+    setWrongTexts(14, "ú");
+  } else {
+    wrongnessEl.textContent = `Chybovost: 0.00`;
+    wrongCharsEl.textContent = `Chybné znaky: `;
+    setCorrectTexts(14);
+  }
+  slowestCharsEl.textContent = `Nejpomalejší znaky: ${getRandomArrayEl(
+    letters[lectionId]
+  )}`;
+}
+
+generateResults();
+
+console.log(" ".repeat(15) + "test");
